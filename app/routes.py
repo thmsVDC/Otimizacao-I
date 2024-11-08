da44fd6d7ea1fc1a1c29ab588e06c5c77f09a8f4
thomasvictor <thomasvictor2909@gmail.com>
2024-11-08 16:19:41 -0300

    Iniciando projeto

---
+++ app/routes.py
@@ -0,0 +1,12 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/result')
+def result_page():
+    return render_template('result.html')
---
+++ config.py
@@ -0,0 +1,3 @@
+class Config:
+    SECRET_KEY = 'sua_chave_secreta_aqui'
+    DEBUG = True
