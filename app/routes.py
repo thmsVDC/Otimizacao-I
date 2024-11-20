37aace7de1703bfc40c4180c3f01fad7314f95ec
thomasvictor <thomasvictor2909@gmail.com>
2024-11-20 19:39:18 -0300

    refazendo o codigo

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,26 @@
+from flask import request, render_template
 from app import app
-from flask import render_template
 
 
-@app.route('/')
-@app.route('/home')
-def home_page():
-    return render_template('home.html')
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('form.html', num_variables=0, num_restrictions=0)
 
-@app.route('/result')
-def result_page():
-    return render_template('result.html')
+
+@app.route('/variables', methods=['POST', 'GET'])
+def variables():
+    num_variables = None
+    num_restrictions = None
+    if request.method == 'POST':
+        num_variables = int(request.form['num_variables'])
+        num_restrictions = int(request.form['num_restrictions'])
+        labels = [chr(65 + i) for i in range(num_variables)]
+        operators = ['<=', '>=']
+
+    return render_template('form.html', num_variables=num_variables, num_restrictions=num_restrictions, labels=labels, operators=operators)
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    variables_data = {key: value for key, value in request.form.items()}
+    return f"Valores recebidos: {variables_data}"
