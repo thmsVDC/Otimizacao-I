496c115d4d6b19ee4fbbee0a8249947c69d07b61
Thomas <141788769+thmsVDC@users.noreply.github.com>
2024-11-27 21:07:28 -0300

    Update shadow_price_process.py

--- app/utils/shadow_price_process.py
+++ app/utils/shadow_price_process.py
@@ -10,10 +10,8 @@ def calculate_shadow_price(result, variables_list, restrictions_list, restrictio
         restrictions_right_modified[i] -= 1
         result_modified = linprog(variables_list, A_ub=restrictions_list, b_ub=restrictions_right_modified,
                                   bounds=[(0, None)] * len(variables_list), method='simplex')
-
+        
         sol_modified = result_modified.fun
-
         shadow_price = sol_original - sol_modified
         shadow_prices.append(-shadow_price)
-
     return shadow_prices
