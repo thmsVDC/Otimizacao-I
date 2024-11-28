fdfd66a70c9232a824598cb9fe57b1642b712991
Thomas <141788769+thmsVDC@users.noreply.github.com>
2024-11-27 21:06:55 -0300

    Update form_utils.py

--- app/utils/form_utils.py
+++ app/utils/form_utils.py
@@ -2,7 +2,6 @@ def dict_to_list(dict):
   list = []
   for key, value in dict.items():
       list.append(value)
-
   return list
 
 def restrictions_dict_to_list(restrictions_dict, num_restrictions):
@@ -11,14 +10,11 @@ def restrictions_dict_to_list(restrictions_dict, num_restrictions):
         _, restriction_idx, var_idx = key.split('_')
         restriction_idx = int(restriction_idx)
         restrictions_list[restriction_idx].append(float(value))
-
     return restrictions_list
 
 def invert_restriction_signs(operators, restrictions, restrictions_right):
     for i, (key, value) in enumerate(operators.items()):
-        print(f"{key}: {value}")
         if value == '>=':
             restrictions[i] = [-value for value in restrictions[i]]
             restrictions_right[i] = -restrictions_right[i]
-
     return restrictions, restrictions_right
