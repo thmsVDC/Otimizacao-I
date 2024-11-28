a4f423aebe3e99fca43f1946969b83bbf76cc646
Thomas <141788769+thmsVDC@users.noreply.github.com>
2024-11-27 21:07:09 -0300

    Update post_optimization_utils.py

--- app/utils/post_optimization_utils.py
+++ app/utils/post_optimization_utils.py
@@ -2,5 +2,4 @@ def restrictions_right_change_dict_to_list(variables_dict):
     restrictions_list = []
     for key, value in variables_dict.items():
         restrictions_list.append(value)
-
     return restrictions_list
